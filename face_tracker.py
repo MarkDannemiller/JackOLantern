92938efc815defc7b98d0ab2666864429c89b009
jwfrancis246 <145225027+jwfrancis246@users.noreply.github.com>
2023-09-20 17:13:28 -0500

    Update face_tracker.py

--- face_tracker.py
+++ face_tracker.py
@@ -54,150 +54,148 @@
 
 
 
-
-
-
-
 import cv2
 import mediapipe
 import random
 import time
-
-class FaceTracker:
-    def __init__(self, video_capture):
-        self.video_capture=video_capture
-        self.mediapipe_face_detection=mediapipe.solutions.face_detection
-        self.mediapipe_drawing=mediapipe.solutions.drawing_utils
-        self.face_entered_square=0
-        self.currently_noted={}
-        self.counter_id=0
-        self.id_options=list(range(10))
-        self.unfilled={}
-        self.text_offset=0
-        self.box_state={}
-        self.target=0
-        self.start=0
-        self.end=0
-        self.x_target=0
-        self.y_target=0
-        self.check=1
-        self.timer=0
-        self.previous1=None
-        self.previous2=None
-        self.currently_seen=None
-        self.test=0
-        self.x_target_final=0
-        self.y_target_final=0
-        self.target_final=0
-        self.start1=0
-        self.in_box=0
-
-    def process(self):
-        ret, frame=self.video_capture.read()
-        #frame=cv2.resize(frame, (640, 480))
-        if not ret:
-            return
-
-        if self.face_entered_square:
-            cv2.rectangle(frame, (250, 150), (400, 300), (255, 0, 0), 1)
-        self.face_entered_square=0
-
-        with self.mediapipe_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.05) as detector_settings:
-            frame=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
-            frame=cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-            location=detector_settings.process(frame)
-            #frame=cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if location.detections:
-                for detection in location.detections:
-                    face_pair=""
-                    initial_bounding = detection.location_data.relative_bounding_box
-                    boundary_box_detection=(int(initial_bounding.xmin * 640),int(initial_bounding.ymin * 480),int(initial_bounding.width * 640),int(initial_bounding.height * 480),)
-                    x = int(640-initial_bounding.xmin*640)
-                    y = int(480 - initial_bounding.ymin*480)
-                    if (250<x<400) and (150<y<350):
-                        self.face_entered_square=1
-
-                    for id_tracker, (x_comparison, y_comparison) in self.currently_noted.items():
-                        x=boundary_box_detection[0]
-                        y=boundary_box_detection[1]
-                        if (abs(x-x_comparison)<100) and (abs(y-y_comparison)<100):
-                            face_pair=id_tracker
-                            self.unfilled[id_tracker]=0
-                            break
-                    if face_pair!="":
-                        self.unfilled[face_pair]=0
-                        x_comparison, y_comparison=self.currently_noted[face_pair]
-                        jitter_coordinate_x=0.9*abs(boundary_box_detection[0]-x_comparison)+x_comparison
-                        jitter_coordinate_y=0.9*abs(boundary_box_detection[1]-y_comparison)+y_comparison
-                        self.currently_noted[face_pair]=(jitter_coordinate_x, jitter_coordinate_y)
-                        if face_pair not in self.box_state:
-                            self.box_state[face_pair]=0
-                        if (250<boundary_box_detection[0]<400) and (150<boundary_box_detection[1]<350):
-                            self.box_state[face_pair]=1
-                        else:
-                            self.box_state[face_pair]=0
-                    else:
-                        if self.id_options:
-                            face_pair=self.id_options[0]
-                            del self.id_options[0]
-                    self.currently_noted[face_pair]=(boundary_box_detection[0], boundary_box_detection[1])
-                    self.unfilled[face_pair]=0
-                    self.text_offset=20
-                    cv2.rectangle(frame, (boundary_box_detection[0], boundary_box_detection[1]), (boundary_box_detection[0] + boundary_box_detection[2], boundary_box_detection[1] + boundary_box_detection[3]), (0, 255, 0), 2)
-                    cv2.putText(frame,f"ID: {face_pair}",(boundary_box_detection[0], boundary_box_detection[1] + self.text_offset),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        for id_tracker in list(self.currently_noted.keys()):
-            self.unfilled[id_tracker]=self.unfilled[id_tracker]+1
-            if self.unfilled[id_tracker]>=45:
-                del self.currently_noted[id_tracker]
-                self.id_options.append(id_tracker)
-
-        if self.start==0:
-            self.start=time.time()
-        current=time.time()
-        self.end=current-self.start
-        if self.end>=5:
-            print("Hi")
-            self.test=0
-            self.currently_seen=list(self.currently_noted.keys())
-            while self.currently_seen:
-                # start1=time.time()
-                self.target=random.choice(self.currently_seen)
-                if self.target!=self.previous1 and self.target!=self.previous2:
-                    self.x_target, self.y_target=self.currently_noted[self.target]
-                    print(f"ID: {self.target}, X: {self.x_target}, Y: {self.y_target}")
-                    self.test=1
-                if (time.time()-self.start>=1):
-                    break
-
-            self.previous2=self.previous1
-            self.previous1=self.target
-            self.start=0
-            self.end=0
-
-            for final_id in list(self.currently_noted.keys()):
-                if final_id==self.target:
-                    self.final_target=self.target
-                    if final_id in self.box_state:
-                        if self.box_state[final_id]:
-                            self.in_box = self.box_state[final_id]
-                            #print(self.in_box)
-                    self.x_target_final=self.x_target
-                    self.y_target_final=self.y_target
-                else:
-                    break
-
-        cv2.imshow('face', frame)
-
-video_capture=cv2.VideoCapture(0)
-tracker=FaceTracker(video_capture)
-
+mediapipe_face_detection = mediapipe.solutions.face_detection
+mediapipe_drawing = mediapipe.solutions.drawing_utils
+cam = cv2.VideoCapture(0)
+face_entered_square = 0
+currently_noted = {}
+counter_id = 0
+id_options = list(range(10))
+unfilled={}
+text_offset=0
+box_state={}
+target=0
+start=0
+end=0
+x_target=0
+y_target=0
+check=1
+timer=0
+previous1=None
+previous2=None
+currently_seen=None
+test=0
+x_target_final=0
+y_target_final=0
+target_final=0
+        
+        
+        
+        
 while True:
-    tracker.process()
+    ret, frame = cam.read()
+    frame = cv2.resize(frame, (640, 480))
+    if not ret:
+        break
+    if (face_entered_square):
+        cv2.rectangle(frame, (250, 150), (400, 300), (0, 255, 0), 1)
+    face_entered_square = 0
+    with mediapipe_face_detection.FaceDetection(model_selection=1, min_detection_confidence=.5) as detector_settings:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = detector_settings.process(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                face_pair=""
+                initial_bounding=detection.location_data.relative_bounding_box
+                #height_scale, width_scale, _ = frame.shape
+                boundary_box_detection=int(initial_bounding.xmin*640), int(initial_bounding.ymin*480), int(initial_bounding.width*640), int(initial_bounding.height*480)
+                x = int(640-initial_bounding.xmin*640)
+                y = int(480-initial_bounding.ymin*480)
+                if (250<x<400) and (150<y<350):
+                    face_entered_square=1
 
-    if cv2.waitKey(5) & 0xFF==ord('x'):
+                    
+                    
+                for id_tracker, (x_comparison, y_comparison) in currently_noted.items():
+                    x, y, _, _ =boundary_box_detection
+                    if (abs(x-x_comparison)<100) and (abs(y-y_comparison)<100):
+                        face_pair=id_tracker
+                        unfilled[id_tracker]=0
+                        break
+                        
+                if face_pair != "":
+                    unfilled[face_pair] = 0
+                    x_comparison, y_comparison = currently_noted[face_pair]
+                    jitter_coordinate_x=.9*(boundary_box_detection[0] - x_comparison)+x_comparison
+                    jitter_coordinate_y=.9*(boundary_box_detection[1] - y_comparison)+y_comparison
+                    currently_noted[face_pair]=(jitter_coordinate_x, jitter_coordinate_y)
+                    if face_pair not in box_state:
+                        box_state[face_pair] = 0
+                    if (250<boundary_box_detection[0]<400) and (150<boundary_box_detection[1]<350):
+                        box_state[face_pair] = 1
+                    else:
+                        box_state[face_pair] = 0
+                else:
+                    if id_options:
+                        face_pair = id_options[0]
+                        del id_options[0]
+                    else:
+                        face_pair = counter_id
+                        counter_id += 1
+                currently_noted[face_pair] = (boundary_box_detection[0], boundary_box_detection[1])
+                unfilled[face_pair] = 0
+                text_offset=20
+                cv2.rectangle(frame, (boundary_box_detection[0], boundary_box_detection[1]), (boundary_box_detection[0] + boundary_box_detection[2], boundary_box_detection[1] + boundary_box_detection[3]), (0, 255, 0), 2)
+                cv2.putText(frame, f"ID: {face_pair}", (boundary_box_detection[0], boundary_box_detection[1]+text_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                #print(f"ID: {face_pair}, X: {boundary_box_detection[0]}, Y: {boundary_box_detection[1]}")
+    for id_tracker in list(currently_noted.keys()):
+        if id_tracker not in unfilled:
+            unfilled[id_tracker] = 1
+        else:
+            unfilled[id_tracker] += 1
+            if unfilled[id_tracker] >= 30:
+                del currently_noted[id_tracker]
+                id_options.append(id_tracker)
+    
+    
+    if (start == 0):
+        start = time.time()
+    current = time.time()
+    end = current - start
+    if (end >= 5):
+        print("Hi")
+        test = 0
+        currently_seen = list(currently_noted.keys())
+        
+
+
+        while test == 0:
+            target = random.choice(currently_seen)
+            if target != previous1 and target != previous2:
+                x_target, y_target = currently_noted[target]
+                print(f"ID: {target}, X: {x_target}, Y: {y_target}")
+                test = 1
+            if (time.time() - start >= 1):
+                break  # Exit the loop if 10 seconds have passed
+
+        previous2 = previous1
+        previous1 = target
+        start = 0
+        end = 0
+        
+        
+        for final_id in list(currently_noted.keys()):
+            if final_id==target:
+                final_target=target
+                x_target_final=x_target
+                y_target_final=y_target
+            else:
+                break
+
+
+    
+    
+        
+        
+    
+    cv2.imshow('face', frame)
+    if cv2.waitKey(5) & 0xFF == 27:
         break
 
-video_capture.release()
+cam.release()
 cv2.destroyAllWindows()
-#frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
