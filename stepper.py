6ce753d5ae28fd49226004950842edac7e732a41
JacksonCase <case.jackson21@gmail.com>
2023-10-02 19:32:36 -0500

    Added stepper code

---
+++ stepper.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from timeit import default_timer as timer
+from multiprocessing import Process, Value
+from random import randrange
+
+gear_ratio = 3
+high_speed = 0.1
+low_speed = 0.025
+s_p_rev = 3200
+min_steps = 160
+low_speed_delay = (1 / (low_speed * s_p_rev * gear_ratio))
+high_speed_delay = (1 / (high_speed * s_p_rev * gear_ratio))
+accel_factor = 1/(((3/low_speed_delay-high_speed_delay))/8)*min_steps
+setpoint = Value('i', 0)
+
+enable_pin = 13
+dir_pin = 19
+step_pin = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(dir_pin, GPIO.OUT)
+GPIO.setup(step_pin, GPIO.OUT)
+
+GPIO.output(enable_pin, GPIO.HIGH)
+
+def set_setpoint(theta):
+    global setpoint
+    setpoint.value = ((theta*gear_ratio)/(360*s_p_rev))
+
+def motor(setpoint):
+    prev_setpoint = 0
+    curr_setpoint = 0
+    total_steps = 0
+    while(1):
+        curr_setpoint = setpoint.value
+        start = timer()
+        if((prev_setpoint - curr_setpoint) not in [-1, 0, 1]):
+            total_steps = abs(setpoint.value)
+        if(setpoint.value != 0 and total_steps != 0):
+            if(setpoint.value < 0):
+                GPIO.output(dir_pin, GPIO.LOW)
+                setpoint.value += 1
+                GPIO.output(enable_pin, GPIO.LOW)
+            elif(setpoint.value > 0):
+                GPIO.output(dir_pin, GPIO.HIGH)
+                setpoint.value -= 1
+                GPIO.output(enable_pin, GPIO.LOW)
+            GPIO.output(step_pin, GPIO.HIGH)
+            GPIO.output(step_pin, GPIO.LOW)
+            delay = 1.5*((accel_factor*abs(setpoint.value))*((1/total_steps)*(abs(setpoint.value))-1)) + low_speed_delay
+            if(delay < high_speed_delay):
+                delay = high_speed_delay
+            prev_setpoint = curr_setpoint
+            while(timer()-start < delay): pass
+        else:
+            GPIO.output(enable_pin, GPIO.HIGH)
+   
+if(__name__ == '__main__'):
+    process = Process(target=motor, args=(setpoint, ))
+    process.start()
+    
+    GPIO.output(enable_pin, GPIO.HIGH)
+    process.terminate()
+    
