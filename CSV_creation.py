52177ef577a676ab5fd690a0d203ec2191e19220
jwfrancis246 <145225027+jwfrancis246@users.noreply.github.com>
2023-10-12 14:45:49 -0500

    Create CSV_creation.py

---
+++ CSV_creation.py
@@ -0,0 +1,88 @@
+import wave
+import pyaudio
+import numpy
+import time
+import csv
+import sys
+
+chunk = 2000
+
+class AudioPlayer:
+    def __init__(self, audio_file_mapping):
+        self.volume_check = time.time()
+        self.volume_list = []
+        self.audio_file_mapping = audio_file_mapping
+        self.p = pyaudio.PyAudio()
+        self.iteration11 = 0
+        self.check = 0
+        self.list=[]
+        self.final_list=[]
+
+    def calculate_volume(self, audio_info, audio_id):
+        if numpy.isnan(audio_info).any():
+            vol = 0
+        else:
+            peaks = numpy.where((audio_info[1:-1] > audio_info[:-2]) & (audio_info[1:-1] > audio_info[2:]))
+
+            if time.time() - self.volume_check >= 0.5:
+                max_peak = numpy.max(audio_info)
+                vol = max_peak / 10000
+                vol = abs(vol)
+                if vol > 1:
+                    vol = 1
+                self.volume_check = time.time()
+                self.list.append(vol)
+            else:
+                vol = 0
+        return vol, self.list
+
+    def play_audio_files(self):
+        for audio_file, audio_id in self.audio_file_mapping.items():
+            self.list.append(audio_id)
+
+            try:   
+                wav = wave.open(audio_file, 'rb')
+                if wav:
+                    stream = self.p.open(format=self.p.get_format_from_width(wav.getsampwidth()), channels=wav.getnchannels(), rate=wav.getframerate(), output=True)
+                    try:
+                        while True:
+                            audio_data = wav.readframes(chunk)
+                            if not audio_data:
+                                break
+
+                            audio_info = numpy.frombuffer(audio_data, dtype=numpy.int16)
+                            if numpy.isnan(audio_info).any():
+                                vol = 0
+                            else:
+                                vol = self.calculate_volume(audio_info, audio_id)
+                            stream.write(audio_data)
+
+                    finally:
+                        print(self.list)
+                        self.final_list.append(self.list)
+                        print(self.final_list)
+                        wav.close()
+                        stream.stop_stream()
+                        stream.close()
+                        self.list=[]
+                else:
+                    break
+            except:
+                with open('audio_data.csv', 'w', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    transposed = numpy.array(self.final_list).T.tolist()
+                    for column in transposed:
+                        writer.writerow(column)
+                sys.exit()
+file_id_mapping = {
+    "C:\\Users\\jwfra\\Desktop\\Lab 2\\wave_test.wav": 0,
+    "C:\\Users\\jwfra\\Desktop\\Lab 2\\wave_test1.wav": 1,
+    "C:\\Users\\jwfra\\Desktop\\Lab 2\\wave_test2.wav": 2,
+    "C:\\Users\\jwfra\\Desktop\\Lab": 4
+}
+
+audio_player = AudioPlayer(file_id_mapping)
+
+while True:
+    volume = audio_player.play_audio_files()
+
